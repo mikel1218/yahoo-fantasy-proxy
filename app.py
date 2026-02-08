13b87a8c2e886bc048167b3f27f4945df44acae0
mikel1218 <mikel1218@yahoo.com>
2026-02-07 20:04:47 -0800

    Update app.py
    Add FanGraphs pitcher skills endpoint

--- app.py
+++ app.py
@@ -15,3 +15,23 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
+from bs4 import BeautifulSoup
+import pandas as pd l
+from io import StringIO
+
+@app.route("/fangraphs/pitcher_skills")
+def fangraphs_pitchers():
+    url = (
+        "https://www.fangraphs.com/leaders.aspx"
+        "?pos=all&stats=pit&type=8&season=2026"
+    )
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    table = soup.find("table")
+    df = pd.read_html(StringIO(str(table)))[0]
+
+    cols = ["Name", "K%", "BB%", "SwStr%", "GB%", "xFIP"]
+    df = df[cols].dropna()
+
+    return df.head(50).to_json(orient="records")
